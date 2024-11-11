ecf186dd38358e1684b44096f09909b8922525a8
Abdumalik606 <abdumaliksaidjalolov2@gmail.com>
2024-11-11 17:28:10 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,22 @@
+let mevalar = ['olma','anor','gilos','banan','kivi']
+
+let a = []
+let b = []
+
+
+
+for (items of mevalar) {
+if(items.includes('a')){
+    a.push(items)
+}else{
+    b.push(items)
+}
+}
+
+console.log(a);
+console.log(mevalar);
+console.log(b);
+
+
+
+
